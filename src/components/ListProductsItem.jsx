a71262ff60f898405081f779b74d8b4d278c741b
Anderson Vallejo <vallejoanderson@gmail.com>
2022-03-09 10:56:00 -0500

    feat: minus and plus items in eachproduct

--- src/components/ListProductsItem.jsx
+++ src/components/ListProductsItem.jsx
@@ -1,6 +1,6 @@
 
 
-import React, {   useState } from 'react';
+import React, { useState } from 'react';
 import { getProductImage } from '../actions/products';
 import { AddItemModal } from './AddItemModal';
 import { Button } from './Button';
@@ -10,14 +10,44 @@ import { useDispatch } from 'react-redux';
 export const ListProductsItem = ({ product:p }) => {
 	const [ showModal, setShowModal ] = useState( false );
 	const dispatch = useDispatch();
+	const [ counter, setCounter ] = useState(0);
+
+	const minusProduct = () => {
+		setCounter( counter => counter - 1 );
+		dispatch({type: types.rm, payload: p});
+	}
+
+	const plusProduct = () => {
+		setCounter( counter => counter + 1 );
+		dispatch({type: types.add, payload: p});
+	}
 
 	return(
 					<div className="border border-gray-200 rounded-lg p-8 shadow hover:shadow-2xl cursor-pointer">
 							<p>{ p.title }</p>
 							<img className="mb-4" alt="product image" height={ 150 } width={ 150 } src={ getProductImage( p.image ) }/>
 							<p className="text-base font-bold text-gray-600 mb-2 pl-2">Price: ${ p. price }</p>
-							<Button fn = { () => { setShowModal( showModal => !showModal ); dispatch({type: types.add, payload: p}); console.log(p)}}
-											text = {'Buy this item'} />
+							{
+								counter > 0
+								?
+								<div className="flex justify-between">
+									<button onClick={ () => { minusProduct() }}>
+										<i class="las la-minus-circle la-2x text-red-500"></i>
+									</button>
+									<div>
+										{ counter }
+									</div>
+									<button onClick={ () => { plusProduct() }}>
+										<i className="las la-plus-circle la-2x text-newblue"></i>
+									</button>
+								</div>
+								:
+								<Button fn = { () => { 
+									setShowModal( showModal => !showModal );
+									plusProduct();
+									console.log(p)}}
+									text = {'Buy this item'} />
+							}
 							<AddItemModal
 														showModal = { showModal }
 														setShowModal = { setShowModal }
