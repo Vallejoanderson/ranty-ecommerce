c9b0531d6ea6d899d189e228620caf310c1f7e25
Steven Ha <47872571+Steven-Ha-88@users.noreply.github.com>
2022-03-09 20:25:14 +0000

    cart-selected (#3)
    * cart-selected
    
    * Delete package-lock.json
    
    * Delete yarn.lock
    
    Co-authored-by: Steven Ha <ming@Stevens-MacBook-Pro.local>
    Co-authored-by: Anderson Vallejo <48649209+Vallejoanderson@users.noreply.github.com>

--- src/components/ListProducts.jsx
+++ src/components/ListProducts.jsx
@@ -7,15 +7,20 @@ import { ListProductsItem } from './ListProductsItem';
 export const ListProducts = () => {
 
 	const products = useSelector( state => state.prods );
-	
+	const cart = useSelector(state => state.cart);
+
 	return(
 					<div className="flex justify-center flex-wrap my-12 gap-8">
 						{
 							products.length > 0 && products.map( p => {
-								return(
-												<ListProductsItem key = { p.id } product = { p } />
-											)
-							} )
+								if(cart.some(el => el.id === p.id)) {
+									return (
+										<ListProductsItem color='border-blue-600' key = { p.id } product = { p } />
+									)
+								} return(
+											<ListProductsItem key = { p.id } product = { p } />
+										)
+						} )
 						}
 					</div>
 	)
